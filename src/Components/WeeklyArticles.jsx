2c54d45000fcd863f9bc0d29487aa12da82874f6
Taimoor Khan <46717905+Taimoorkhan1122@users.noreply.github.com>
2021-02-06 14:42:28 +0500

    Feat:  Github Action added

--- src/Components/WeeklyArticles.jsx
+++ src/Components/WeeklyArticles.jsx
@@ -3,23 +3,24 @@ import React from "react";
 const WeeklyArticles = ({ data: { weeklyArticles } }) => {
   console.log(weeklyArticles);
   return (
-    <div className="wk-art">
+    <div className="wk-art my-10">
       <h3>Weekly Articles</h3>
-      <div className="wk-art-container w-auto flex ">
-        <div className="img-container ">
-          <img src="./assets/wk-article-1.png" alt="footbaal" />
-        </div>
-        <div className="content w-1/3 px-5 ">
-          <h3>The best games in this year</h3>
-          <p className="italic">11 January 2020</p>
-          <p>
-            Get ready for create awesome football game, climb the highest rank
-            in your city.
-          </p>
-          <div className="icon ">
-            <img src="./assets/arrow-right-circle.svg" alt="icon" />
+      <div className="container flex justify-between px-0">
+        {weeklyArticles.map((article) => (
+          <div className="wk-art-container w-auto flex ">
+            <div className="img-container ">
+              <img src="./assets/wk-article-1.png" alt="footbaal" />
+            </div>
+            <div className="content w-9/12 pl-5 ">
+              <h3>{article.heading}</h3>
+              <p className="italic">{article.date}</p>
+              <p>{article.content}</p>
+              <div className="icon ">
+                <img src="./assets/arrow-right-circle.svg" alt="icon" />
+              </div>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
